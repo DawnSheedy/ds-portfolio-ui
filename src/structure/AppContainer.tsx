76bfc3c3aa0ea59ee46710b19df0a3092b504036
DawnSheedy <dawn@dawnsheedy.com>
2024-02-06 23:09:22 -0800

    Make page obey viewport size

--- src/structure/AppContainer.tsx
+++ src/structure/AppContainer.tsx
@@ -6,7 +6,7 @@ export interface ContentSafeColumnProps {
 
 export const AppContainer = ({ children }: ContentSafeColumnProps) => {
   return (
-    <div className="bg-slate-100 dark:bg-slate-950 text-slate-900 dark:text-slate-100 h-screen flex flex-col justify-between">
+    <div className="bg-slate-100 dark:bg-slate-950 text-slate-900 dark:text-slate-100 h-dvh flex flex-col justify-between">
       <div className="container mx-auto">{children}</div>
       <EnvIndicator />
     </div>
